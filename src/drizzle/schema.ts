693d95d373a0cf5bd331a0418e880d0c0f20a8ab
Garrett Winter <Garrett_Winter@hotmail.com>
2024-09-02 21:54:12 -0500

    Defined the initial schema for the task table.

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,7 +1,20 @@
-import { pgTable, uuid, varchar, timestamp } from "drizzle-orm/pg-core"
+import { pgTable, uuid, varchar, timestamp, integer, smallint, date } from "drizzle-orm/pg-core";
+
 export const UserTable = pgTable("user_table", {
-    userId: uuid("userId").primaryKey().defaultRandom(),
+    userId: uuid("user_id").primaryKey().defaultRandom(),
     username: varchar("username", { length: 255 }).notNull(),
-    dateCreated: timestamp("dateCreated").defaultNow(),
-    lastUpdated: timestamp("lastUpdated").defaultNow(),
+    dateCreated: timestamp("date_created").defaultNow(),
+    lastUpdated: timestamp("last_updated").defaultNow(),
+});
+
+export const TaskTable = pgTable("task_table", {
+    taskId: uuid("task_id").primaryKey().defaultRandom(),
+    taskName: varchar("task_name").notNull(), ///// SHould this have a set max length?
+    listId: varchar("list_id").notNull(), //// This should be updated to a array of varchars
+    taskRank: smallint("task_rank"),
+    taskStatus: varchar("task_status"),
+    assignee: varchar("assignee"), //// Change into a array
+    dueDate: date("due_date"),
+    dateCreated: timestamp("date_created").defaultNow(),
+    lastUpdated: timestamp("last_updated").defaultNow(),
 });
